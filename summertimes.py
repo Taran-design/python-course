05f5538e3de257fac2ec610f5847a0ac9999c906
Taran-design <82270628+Taran-design@users.noreply.github.com>
2026-06-06 08:46:11 +0100

    Add files via upload

---
+++ summertimes.py
@@ -0,0 +1,8 @@
+weather_temp = int(input("What is the temperature right now? :"))
+
+if weather_temp > 20 :
+    print("You can wear shorts and t-shirts!")
+elif weather_temp > 10:
+    print("Its a little colder,wear tracksuit and your jacket")
+else:
+    print("Its quite cold, wear a puffer jacket with trousers!")
