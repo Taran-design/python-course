746c6b58b712b16ced05dd31360102e91c84a9dd
Taran-design <82270628+Taran-design@users.noreply.github.com>
2026-06-20 08:07:57 +0100

    Add files via upload

---
+++ checkage.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+if age >= 10:
+    if age<= 20:
+        print("Your age is between 10 and 20")
+    else:
+        print("Your age is more than 20")
+else:
+    print("your age is less than 10")
