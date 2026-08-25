5bcaada07c8639a4b9b4fe426a350742f36d4ce0
Taran-design <82270628+Taran-design@users.noreply.github.com>
2026-08-25 16:40:18 +0100

    Add files via upload

---
+++ snacks.py
@@ -0,0 +1,18 @@
+snacks1 = {"crisps","chocalte","banana"}
+snacks2 = {"chocolate","sweets","banana"}
+
+snacks1.add("popcorn")
+print("Snacks 1:", snacks1)
+
+shared_snacks = snacks1.intersection(snacks2)
+print("Shared snacks:", shared_snacks)
+
+number = [2,5,3,2,4]
+
+number.append(6)
+
+print("Snack count:", number)
+
+print(count.counts(2))
+counts.reverse()
+print(counts)
