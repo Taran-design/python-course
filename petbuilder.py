665383b689e8368422b480dc566df5a9b961b7b1
Taran-design <82270628+Taran-design@users.noreply.github.com>
2026-09-19 08:14:06 +0100

    Add files via upload

---
+++ petbuilder.py
@@ -0,0 +1,11 @@
+class Pet:
+    def __init__(self, name, age, animal):
+        self.name = name
+        self.age = age
+        self.animal = animal
+
+pet1 = Pet("Max", 3, "Dog")
+
+print(pet1.name)
+print(pet1.age)
+print(pet1.animal)
